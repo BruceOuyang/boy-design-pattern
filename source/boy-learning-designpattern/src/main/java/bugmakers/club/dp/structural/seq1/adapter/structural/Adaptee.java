4c3aacf624b7babcf1b2de85948594986a63a35b
BruceOuyang <oiiopro@live.cn>
2018-03-09 13:24:54 +0800

    adapter-pattern  -  add structural

---
+++ source/boy-learning-designpattern/src/main/java/bugmakers/club/dp/structural/seq1/adapter/structural/Adaptee.java
@@ -0,0 +1,13 @@
+package bugmakers.club.dp.structural.seq1.adapter.structural;
+
+/**
+ * @Description: 适配者
+ * @Author: Bruce
+ * @Datetime: 2018/3/9 11:09
+ */
+public class Adaptee {
+
+    public void specialRequest(){
+        System.out.println("适配者在处理业务");
+    }
+}
---
+++ source/boy-learning-designpattern/src/main/java/bugmakers/club/dp/structural/seq1/adapter/structural/Target.java
@@ -0,0 +1,11 @@
+package bugmakers.club.dp.structural.seq1.adapter.structural;
+
+/**
+ * @Description:
+ * @Author: Bruce
+ * @Datetime: 2018/3/9 11:08
+ */
+public abstract class Target {
+
+    public abstract void request();
+}
