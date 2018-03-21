efd70fb39a59df2bdf2fd847a54d48aff6ad5cab
BruceOuyang <oiiopro@live.cn>
2018-03-21 18:05:47 +0800

    interpreter-pattern - add practice

--- source/boy-learning-designpattern/src/test/java/bugmakers/club/dp/behavioral/seq03/interpreter/demooptimized/InterpreterContextDemoClient.java
+++ source/boy-learning-designpattern/src/test/java/bugmakers/club/dp/behavioral/seq03/interpreter/demooptimized/InterpreterContextDemoClient.java
@@ -11,7 +11,7 @@ public class InterpreterContextDemoClient {
      * 程序启动入口
      */
     public static void main(String[] args) {
-        String text = "LOOP 2 PRINT 杨过 SPACE SPACE PRINT 小龙女 BREAK END PRINT 郭靖 SPACE SPACE PRINT 黄蓉";
+        String text = "LOOP 2 PRINT 杨过 SPACE SPACE PRINT 小龙女 BREAK END PRINT 郭靖 SPACE SPACE PRINT 黄蓉 BREAK END";
 
         Context context = new Context(text);
 
