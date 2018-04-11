a105e457b442bda9ef0041ba45783a2cf005e706
BruceOuyang <oiiopro@live.cn>
2018-04-11 17:46:32 +0800

    add review & practices

---
+++ source/boy-learning-designpattern/src/main/java/bugmakers/club/dp/review/soccerandpattern/creational/AbstractBallFactory.java
@@ -0,0 +1,11 @@
+package bugmakers.club.dp.review.soccerandpattern.creational;
+
+/**
+ * @Description: 工厂方法模式：近年来大型足球比赛（世界杯和欧洲杯）的指定用球都是阿迪达斯的（据说是签了合同的），当然Adidas足球是由Adidas公司生产的，除此之外，Nike公司也生产Nike足球，KAPPA（背靠背）公司也生产背靠背足球，足球生产商是工厂，足球是产品。增加一种新的足球品牌，对应需要增加一个新的生产商。
+ * @Author: Bruce
+ * @Datetime: 2018/4/11 17:43
+ */
+public class AbstractBallFactory {
+
+
+}
